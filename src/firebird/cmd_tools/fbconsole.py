3a10b00037fd10238f9e09631f5fc3cf5fce209b
Stone Zhong <stone.zhong@gmail.com>
2023-05-06 14:15:02 -0700

    fix fbconsole

--- src/firebird/cmd_tools/fbconsole.py
+++ src/firebird/cmd_tools/fbconsole.py
@@ -8,26 +8,10 @@
 # The args format is the same as you pass to manage.py in a regular django app
 ####################################################################################
 
-import argparse
 import os
 import sys
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Firebird Console'
-    )
-    parser.add_argument(
-        "-cd", "--config-dir", type=str, default=os.path.expanduser("~/.firebird"), required=False,
-        help="Configuration directory"
-    )
-    parser.add_argument(
-        "-a", "--ip-address", type=str, default="0.0.0.0", help="binding ip address"
-    )
-    parser.add_argument(
-        "-p", "--port", type=str, default="80", help="binding port"
-    )
-    args = parser.parse_args()
-
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firebirdconsole.firebirdconsole.settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
