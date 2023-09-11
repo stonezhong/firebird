9cb141b477625aadacb2e98f5fbdc3d5c9eb7450
Stone Zhong <stone.zhong@gmail.com>
2023-09-10 20:43:03 -0700

    update

--- examples/getting-started/console/build.sh
+++ examples/getting-started/console/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t firebird-console .
+docker build --progress=plain -t firebird-console .
 docker tag firebird-console iad.ocir.io/idrnu3akjpv5/firebird-console
 docker push iad.ocir.io/idrnu3akjpv5/firebird-console
